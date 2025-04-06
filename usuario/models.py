4c76c6627e30812c5952af804a9b90eb0488262e
Igor Gassen <igor.gassen@awright.com.br>
2025-04-06 09:54:44 -0300

    Reformulação da tabela Usuario. Caso dê problema, dropar e recriar o banco mente_saudavel

--- usuario/models.py
+++ usuario/models.py
@@ -1,29 +1,22 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
 class UsuarioManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('Email é obrigatório.')
+        
         email = self.normalize_email(email)
         usuario = self.model(email=email, **extra_fields)
         usuario.set_password(password)
         usuario.save(using=self._db)
         return usuario
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        return self.create_user(email, password, **extra_fields)
-
-class Usuario(AbstractBaseUser, PermissionsMixin):
+    
+class Usuario(AbstractBaseUser):
     email = models.EmailField(max_length=100, unique=True)
     nome = models.CharField(max_length=100)
     data_nascimento = models.DateField()
     sexo = models.CharField(max_length=1, choices=[('M', 'Masculino'), ('F', 'Feminino')])
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
 
     objects = UsuarioManager()
 
