e4a0effd52c5538d7179596f6fde726a948750a1
Igor Gassen <igor.gassen@awright.com.br>
2025-04-13 19:58:22 -0300

    Pequenos ajustes pré-apresentação

--- resultado/views.py
+++ resultado/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def resultado(request):
-    pontuacao = request.GET.get('pontuacao')
+    pontuacao = request.GET.get('pontuacao', 0)
     return render(request, 'resultado.html', {'pontuacao': pontuacao})
