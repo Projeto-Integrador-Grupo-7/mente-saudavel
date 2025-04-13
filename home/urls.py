42b32747cff482234ef72527ebc8b10509f2bcb6
Hugo Holler <hugoholler@hotmail.com>
2025-04-13 19:38:50 -0300

    Cards de navegacao da home feitos

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('home/', lambda request: redirect('home')),
+    path('', views.home, name='home')
 ]
